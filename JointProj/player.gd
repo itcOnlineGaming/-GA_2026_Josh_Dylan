3e5843fb03211761400390d948ba56703e8e4d7f
unknown <jalsybury@gmail.com>
2026-01-12 13:35:23 +0000

    teleport function added

--- JointProj/player.gd
+++ JointProj/player.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 signal hit
 
+@export var teleport_cooldown_time := 100 # seconds
+var teleport_timer := 0
+
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
@@ -9,8 +12,16 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
+func _input(event):
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
 
 func _process(delta):
+	if teleport_timer > 0:
+		teleport_timer -= delta
+	
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed(&"move_right"):
 		velocity.x += 1
@@ -20,6 +31,12 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed(&"move_up"):
 		velocity.y -= 1
+	if Input.is_action_pressed(&"Teleport"):
+		if teleport_timer == 0:
+			position = get_global_mouse_position()
+			teleport_timer = teleport_cooldown_time
+
+	print(teleport_timer)
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
